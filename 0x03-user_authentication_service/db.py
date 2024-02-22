f803163ba0f900eeb39324082e879cfbf6c1da57
Smart  Raysam <engsamtob@yahoo.com>
2024-02-22 21:25:59 +0100

    Task 2 find user task 3 : update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -12,12 +12,10 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -25,8 +23,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -41,4 +38,29 @@ class DB:
         self._session.commit()
 
         return user
-    
+
+    def find_user_by(self, **kwargs) -> User:
+        """Method takes keyword & returns filtered results frm users table"""
+
+        if kwargs is None:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Method to locate user and update it in the database"""
+
+        userId = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if not hasattr(userId, key):
+                raise ValueError
+
+            setattr(userId, key, value)
+
+        self._session.commit()
